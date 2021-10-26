050344c75d74d03983084da23fde6b54ce08b0f4
oct-sky-out <kms3335k@naver.com>
2021-10-27 01:06:00 +0900

    파이어베이스 추가, 리액트 아이콘 패키지 추가

--- next_reservation/__tests__/components/Header/Header.test.tsx
+++ next_reservation/__tests__/components/Header/Header.test.tsx
@@ -1,6 +1,5 @@
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { useState } from 'react';
 import Header from '../../../components/Header/Header';
 
 describe('<Header/>', () => {
@@ -10,8 +9,6 @@ describe('<Header/>', () => {
     expect(header.getByText('회원가입')).toBeInstanceOf(HTMLButtonElement);
   });
   test('헤더 렌더링 | 회원가입 버튼 클릭 이벤트 발생 -> 모달창 렌더링', () => {
-    // const [mockActiveModal, setMockActiveModal] = useState(true);
-
     const header = render(<Header />);
     const signUpBtn = header.getByText('회원가입');
     fireEvent.click(signUpBtn);
