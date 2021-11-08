d461e3d36c0bda39d13adec63d9143b853a05ea3
oct-sky-out <kms3335k@naver.com>
2021-11-08 23:26:05 +0900

    테스팅 작성

--- next_reservation/__tests__/components/Header/HeaderUserProfile.test.tsx
+++ next_reservation/__tests__/components/Header/HeaderUserProfile.test.tsx
@@ -1,7 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { isVisible } from '@testing-library/user-event/dist/utils';
 import { Provider } from 'react-redux';
 import { useMockStore } from '../../../store/index';
-import { useDispatchMock } from '../../../__mocks__/auth/authMocks';
+import {
+  useDispatchMock,
+  useSelectorMock,
+  mockStoreValue,
+} from '../../../__mocks__/auth/authMocks';
 
 // 실제 파일의 import 할 패키지
 import React, { useState } from 'react';
@@ -31,25 +37,28 @@ const HeaderUserProfile = () => {
       >
         <button
           type="button"
+          data-testid="user-profile-picture-button"
           className="shadow-xl rounded-full py-3 px-6 header-user-profile"
           onClick={() => {
             setIsUserMenuOpened(!isUserMenuOpened);
           }}
         >
           <img
-            src={userProfile.userPicture.src}
+            src="../../public/static/user/default_user_picture.png"
             className="header-user-profile-image"
           />
         </button>
         {isUserMenuOpened && (
           <>
             <button
+              data-testid="user-profile-open-button"
               type="button"
               className="shadow-xl rounded-full py-3 px-6 header-user-profile-management"
             >
               계정
             </button>
             <button
+              data-testid="user-logout-button"
               type="button"
               className="shadow-xl rounded-full py-3 px-6 header-user-logout"
             >
@@ -66,20 +75,38 @@ const HeaderUserProfile = () => {
 beforeEach(() => {
   const dispatchMock = useDispatchMock;
   store.dispatch = dispatchMock;
+  useSelectorMock.mockImplementation((selector) =>
+    selector({ user: mockStoreValue })
+  );
 });
 
-test('회원가입 모달 렌더링', () => {
-  useDispatchMock.mockReturnValue(jest.fn());
-
+test('유저 프로필 버튼을 누르면 메뉴가 열림', async () => {
   render(
     <Provider store={store}>
       <HeaderUserProfile />
     </Provider>
   );
+
+  const userPictureButton = await screen.findByTestId<HTMLButtonElement>(
+    'user-profile-picture-button'
+  );
+
+  const userProfileOpenBtn = await screen.findByTestId<HTMLButtonElement>(
+    'user-profile-open-button'
+  );
+  const userLogoutBtn = await screen.findByTestId<HTMLButtonElement>(
+    'user-logout-button'
+  );
+
+  expect(isVisible(userProfileOpenBtn)).toEqual(false);
+  expect(isVisible(userLogoutBtn)).toEqual(false);
+
+  userEvent.click(userPictureButton);
+
+  expect(isVisible(userProfileOpenBtn)).toEqual(true);
+  expect(isVisible(userLogoutBtn)).toEqual(true);
 });
 
-test('유저 프로필 버튼을 누르면 메뉴가 열림');
+test('유저 프로필이 열린 상태에서 프로필 버튼을 누르면 메뉴가 닫힘', () => {});
 
-test('유저 프로필이 열린 상태에서 프로필 버튼을 누르면 메뉴가 닫힘');
-
-test('유저 프로필이 열린 상태에서 메뉴 이외의 화면을 누르면 메뉴가 닫힘');
+test('유저 프로필이 열린 상태에서 메뉴 이외의 화면을 누르면 메뉴가 닫힘', () => {});
