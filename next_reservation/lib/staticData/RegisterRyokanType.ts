21134f5862da62dea3277451d168a2b5b2649c46
oct-sky-out <kms3335k@naver.com>
2021-11-13 01:46:14 +0900

    selector options 선언

---
+++ next_reservation/lib/staticData/RegisterRyokanType.ts
@@ -0,0 +1,11 @@
+export const RyokanType: { [key: string]: string } = {
+  Japanese: '화실',
+  Western: '양실',
+  'Japanese-Western': '화양실',
+};
+
+export const BuildingType: { [key: string]: string } = {
+  Rental_Cottage: '독채 건물',
+  Private_Room: '개인실',
+  Shared_Room: '공용실(도미토리, 10명이상)',
+};
