ebe62bb8488cd8c54d13b818a8afb273a7bbb52d
oct-sky-out <kms3335k@naver.com>
2021-11-16 00:22:41 +0900

    료칸등록 1단계 코드 리팩터링, 2단계 침실등록 기본 컴포넌트 생성

--- next_reservation/components/Register/RegisterRyokan/RegisterRyokan.tsx
+++ next_reservation/components/Register/RegisterRyokan/RegisterRyokan.tsx
@@ -1,5 +1,4 @@
-import { GetServerSideProps } from 'next';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from '../../../store';
 import { registerRyokanActions } from '../../../store/registerRyokan';
@@ -25,11 +24,6 @@ const RegisterRyokan: React.FC<IPorps> = ({ priviousHref }) => {
       isBuiltInOnsen: selector.registerRyokan.isBuiltInOnsen,
     })
   );
-  //* useStates
-  const [ryokanAndBuildingType, setryokanAndBuildingType] = useState({
-    ryokan: '료칸유형을 선택해주세요.',
-    building: '건물유형을 선택해주세요.',
-  });
   //* useCallbacks
   const selectedRyokanTypeOrBuildingType = useCallback(
     ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) =>
@@ -38,25 +32,14 @@ const RegisterRyokan: React.FC<IPorps> = ({ priviousHref }) => {
           (key) => RyokanTypesOrBuildingTypes[key] === value
         );
         if (typeKey) {
-          if (typeKey in RyokanTypes) {
-            setryokanAndBuildingType({
-              ...ryokanAndBuildingType,
-              ryokan: value,
-            });
+          if (typeKey in RyokanTypes)
             dispatch(registerRyokanActions.setRyokanType(typeKey));
-          }
-          if (typeKey in BuildingTypes) {
-            setryokanAndBuildingType({
-              ...ryokanAndBuildingType,
-              building: value,
-            });
+          if (typeKey in BuildingTypes)
             dispatch(registerRyokanActions.setBuildingType(typeKey));
-          }
         }
       },
     [buildingType, ryokanType]
   );
-
   const selectedBuiltInOnsen = useCallback(
     ({ target: { checked } }: React.ChangeEvent<HTMLInputElement>) => {
       dispatch(registerRyokanActions.setIsBuiltInOnsen(checked));
@@ -72,34 +55,48 @@ const RegisterRyokan: React.FC<IPorps> = ({ priviousHref }) => {
             호스팅 할 료칸유형을 선택해주세요.
           </h1>
         </div>
-        <div className="w-full col-start-2 flex flex-col justify-center p-10">
-          <span className="text-black mb-3">1. 료칸유형</span>
-          <Selector
-            className="mb-5"
-            onChange={(e) => selectedRyokanTypeOrBuildingType(e)(RyokanTypes)}
-            value={ryokanAndBuildingType.ryokan}
-            disableOption="료칸유형을 선택해주세요."
-            options={Object.values(RyokanTypes)}
-          />
-          <span className="text-black mb-3">2. 건물유형</span>
-          <Selector
-            className="mb-5"
-            onChange={(e) => selectedRyokanTypeOrBuildingType(e)(BuildingTypes)}
-            value={ryokanAndBuildingType.building}
-            disableOption="건물유형을 선택해주세요."
-            options={Object.values(BuildingTypes)}
-          />
-          <div className="list-group mb-5 flex justify-center">
-            <span className="text-black mb-3">3. 객실 내 온천여부</span>
-            <label className="list-group-item">
-              <input
-                id="built-in-onsen"
-                className="form-check-input m-0 mr-1 "
-                type="checkbox"
-                onChange={selectedBuiltInOnsen}
-              />
-              객실 내부에 온천이 있습니까?
-            </label>
+        <div className="w-full col-start-2 flex flex-col justify-center items-center">
+          <div className="w-1/3">
+            <span className="text-black mb-3 inline-block">1. 료칸유형</span>
+            <Selector
+              className="mb-5 h-20"
+              onChange={(e) => selectedRyokanTypeOrBuildingType(e)(RyokanTypes)}
+              value={RyokanTypes[ryokanType] || '료칸유형을 선택해주세요.'}
+              disableOption="료칸유형을 선택해주세요."
+              options={Object.values(RyokanTypes)}
+            />
+          </div>
+          <div className="w-1/3">
+            <span className="text-black mb-3 inline-block">2. 건물유형</span>
+            <Selector
+              className="mb-5 h-20"
+              onChange={(e) =>
+                selectedRyokanTypeOrBuildingType(e)(BuildingTypes)
+              }
+              value={BuildingTypes[buildingType] || '건물유형을 선택해주세요.'}
+              disableOption="건물유형을 선택해주세요."
+              options={Object.values(BuildingTypes)}
+            />
+          </div>
+          <div className="w-1/3">
+            <div className="list-group mb-5 flex justify-center">
+              <span className="text-black mb-3 inline-block">
+                3. 객실 내 온천여부
+              </span>
+              <label className="list-group-item h-10 items-center ">
+                <div>
+                  <input
+                    id="built-in-onsen"
+                    className="form-check-input mr-3 my-auto inline-block"
+                    type="checkbox"
+                    onChange={selectedBuiltInOnsen}
+                  />
+                  <span className="my-auto inline-block">
+                    객실 내부에 온천이 있습니까?
+                  </span>
+                </div>
+              </label>
+            </div>
           </div>
           <RegisterFooter
             isValild={!(ryokanType && buildingType)}
---
+++ next_reservation/pages/room/register/bedrooms.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const bedrooms: NextPage = () => {
+  return <div>Enter</div>;
+};
+
+export default bedrooms;
