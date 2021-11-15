a82047e3467c859ace75b7a579a2fc6fef47da59
oct-sky-out <kms3335k@naver.com>
2021-11-16 02:35:31 +0900

    료칸 매 단계 등록 애니메이션 효과적용

--- next_reservation/styles/components/Register/RegisterRyokan.tsx
+++ next_reservation/styles/components/Register/RegisterRyokan.tsx
@@ -10,8 +10,25 @@ const RegisterRyokanStyle = styled.div`
   .left-side-description {
     background: linear-gradient(45deg, rgba(0, 50, 119), rgba(0, 220, 150));
   }
+  .regist-proceduer-text,
+  .register-form {
+    animation-name: fadeInAndUpForm;
+    animation-duration: 1s;
+    animation-iteration-count: none;
+  }
   .list-group-item {
     display: flex;
+  }
+
+  @keyframes fadeInAndUpForm {
+    0% {
+      transform: translateY(25%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
   }
 `;
 
