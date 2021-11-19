222c77500156d8ac482750e6ba019538a28bca7a
oct-sky-out <kms3335k@naver.com>
2021-11-20 05:27:26 +0900

    [료칸등록 2단계 개발] 료칸 등록 2단계 침실 정보등록 페이지 개발. 침실등록 기본정보 화면 디자인

---
+++ next_reservation/components/Register/RegisterRyokanBedrooms/RegisterRyokanBeedrooms.tsx
@@ -0,0 +1,134 @@
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { v4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { useSelector } from '../../../store';
+import { registerRyokanActions } from '../../../store/registerRyokan';
+import { registerFormValidAction } from '../../../store/registerFormIsValid';
+import Selector from '../../../components/common/Selector';
+import {
+  BedTypes,
+  BedroomCount,
+} from '../../../lib/staticData/RegisterRyokanBedrooms';
+
+const RegisterRyokanBeedrooms = () => {
+  const dispatch = useDispatch();
+  const { bedroomList, bedroomCount, personnel, isFormValid } = useSelector(
+    (selector) => ({
+      bedroomList: selector.registerRyokan.bedrooms.bedroomList,
+      bedroomCount: selector.registerRyokan.bedrooms.bedroomCount,
+      personnel: selector.registerRyokan.bedrooms.personnel,
+      isFormValid: selector.registerIsValid.isValid,
+    })
+  );
+
+  useEffect(() => {
+    if (bedroomList && bedroomCount && personnel) {
+      !isFormValid && dispatch(registerFormValidAction.setValid(true));
+    }
+    if (!(bedroomList && bedroomCount && personnel)) {
+      isFormValid && dispatch(registerFormValidAction.setValid(false));
+    }
+  }, [bedroomList, bedroomCount, personnel]);
+
+  //* useCallbacks
+  const personnelAddOrSub = useCallback(
+    ({ currentTarget: { value } }: React.MouseEvent<HTMLButtonElement>) => {
+      if (value === 'add')
+        dispatch(registerRyokanActions.setPersonnel(personnel + 1));
+      if (value === 'sub')
+        dispatch(registerRyokanActions.setPersonnel(personnel - 1));
+    },
+    [personnel]
+  );
+  const selectBedroomCounted = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
+      const count = value.match(/([\d])+/g);
+      if (count) dispatch(registerRyokanActions.setBedroomCount(+count[0]));
+    },
+    [bedroomCount]
+  );
+  //* useMemo
+  const getBedroomList = useMemo(
+    () =>
+      bedroomList.map((bedrooms, bedroomNumber) => {
+        return (
+          <div key={v4()} className="text-black py-5 px-3">
+            <div className="flex flex-col relative">
+              <span className="text-xl inline-block">
+                {bedroomNumber + 1}번 침실
+              </span>
+              {bedrooms.map((bedroom) => {
+                return (
+                  <span className="text-base inline-block" key={v4()}>
+                    {BedTypes[bedroom.bedType]} {bedroom.count}개
+                  </span>
+                );
+              })}
+              <button className="absolute right-0 top-1/3 btn  btn-outline-success">
+                침대 추가하기
+              </button>
+            </div>
+          </div>
+        );
+      }),
+    [bedroomList, bedroomCount]
+  );
+
+  return (
+    <div className="w-full col-start-2 register-form animate-fadeInAndUpForm space-y-5 mx-0 my-auto">
+      <div className="w-1/2 my-0 mx-auto">
+        <span className="text-black mb-3 inline-block text-2xl">
+          최대 숙박 인원
+        </span>
+        <div className="text-black mb-5 space-x-3">
+          <button
+            className={`w-10 h-10 border-2 ${
+              !personnel ? 'border-gray-500 text-gray-500' : 'border-emerald'
+            }  rounded-full`}
+            value="sub"
+            onClick={personnelAddOrSub}
+            disabled={!personnel}
+          >
+            -
+          </button>
+          <span>{personnel}</span>
+          <button
+            className="w-10 h-10 border-2 border-emerald rounded-full"
+            value="add"
+            onClick={personnelAddOrSub}
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div className="w-1/2 my-0 mx-auto">
+        <span className="text-black mb-3 inline-block text-2xl">
+          게스트가 사용 가능한 침실개수를 정해주세요.
+        </span>
+        <Selector
+          className="mb-5 h-20 ryokan-bedroom-count-selector"
+          disableOption="침실개수를 선택해주세요."
+          onChange={selectBedroomCounted}
+          value={`침실 ${bedroomCount}개`}
+          options={BedroomCount}
+        />
+      </div>
+      <div className="w-1/2 my-0 mx-auto">
+        <div className="list-group mb-5 flex justify-center">
+          <span className="text-black mb-3 inline-block text-2xl">
+            침대유형
+          </span>
+          <span className="text-black mb-3 inline-block text-lg">
+            모든 게스트가 편안하게 숙박할 수 있도록 침대가 충분히 구비되어있는지
+            확인해주세요.
+          </span>
+          <div className="divide-solid divide-y divide-gray-300 border-t border-b border-solid border-gray-300">
+            {getBedroomList}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterRyokanBeedrooms;
