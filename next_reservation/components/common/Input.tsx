1459354de6fc6c3d97accc1cfaefbab73b590372
oct-sky-out <kms3335k@naver.com>
2021-12-03 01:29:46 +0900

    [기타] 커스텀 Input태그 Props 속성 변경

--- next_reservation/components/common/Input.tsx
+++ next_reservation/components/common/Input.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import CommonInputStyle from '../../styles/common/Input';
 
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  icon: JSX.Element | null;
+  icon?: JSX.Element;
 }
 const Input: React.FC<IProps> = ({ icon, className, ...props }) => {
   return (
     <CommonInputStyle iconExist={!!icon}>
       <div className="input-wrapper">
         <input className={`form-control ${className}`} {...props} />
-        <div className="icon-wrapper">{icon}</div>
+        <div className="icon-wrapper">{icon ? icon : null}</div>
       </div>
     </CommonInputStyle>
   );
