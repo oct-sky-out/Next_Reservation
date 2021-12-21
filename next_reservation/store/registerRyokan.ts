077e4d836b9cbe9be6e9d041fc80815ec0cd4978
oct-sky-out <kms3335k@naver.com>
2021-12-21 14:43:47 +0900

    [추가기능 3번] 료칸 예약 시작 종료일자 리듀서 제작, 테스팅

--- next_reservation/store/registerRyokan.ts
+++ next_reservation/store/registerRyokan.ts
@@ -50,6 +50,7 @@ const initialState: IRyokanType = {
   title: '',
   description: '',
   pricePerDay: '',
+  date: { openDate: new Date(Date.now()), closeDate: new Date(Date.now()) },
 };
 
 const registerRyokanSlice = createSlice({
@@ -383,6 +384,24 @@ const registerRyokanSlice = createSlice({
       },
       reducer: (state, action: PayloadAction<string>) => {
         return { ...state, pricePerDay: action.payload };
+      },
+    },
+    setOpenDate: {
+      prepare: (openDate: Date) => ({ payload: openDate }),
+      reducer: (state, action: PayloadAction<Date>) => {
+        return {
+          ...state,
+          date: { ...state.date, openDate: action.payload },
+        };
+      },
+    },
+    setCloseDate: {
+      prepare: (closeDate: Date) => ({ payload: closeDate }),
+      reducer: (state, action: PayloadAction<Date>) => {
+        return {
+          ...state,
+          date: { ...state.date, closeDate: action.payload },
+        };
       },
     },
   },
