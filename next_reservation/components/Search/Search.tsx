a464af1105249880c22223b1d55a7839428e328e
oct-sky-out <kms3335k@naver.com>
2022-02-04 21:30:35 +0900

    [기능구현] 검색결과 제작완료

--- next_reservation/components/Search/Search.tsx
+++ next_reservation/components/Search/Search.tsx
@@ -1,13 +1,12 @@
-import { useCallback, useRef, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector } from '@/store/index';
-import { searchResultsRoomsActions } from '@/store/searchResultsRyokans';
 import axios from '@/lib/api';
-import { IRyokanType } from '@/types/reduxActionTypes/ReduxRegiserRyokanType';
-import SearchItem from './SearchItem';
 import { v4 } from 'uuid';
+import Loader from 'react-loader-spinner';
+import SearchItem from './SearchItem';
 import useIntersectionObserver from '../hooks/useIntersectionObserver';
 import SearchFilter from './SearchFilter';
+import { IRyokanType } from '@/types/reduxActionTypes/ReduxRegiserRyokanType';
 
 const Search = () => {
   //* redux
@@ -45,7 +44,7 @@ const Search = () => {
         convenienceSpaces: searchRoomFilter.filter.filterConvenienceSpaces,
         priceMin: searchRoomFilter.filter.filterPricePerDay.min,
         priceMax: searchRoomFilter.filter.filterPricePerDay.max,
-        ryokanType: searchRoomFilter.filter.filterRyokanType,
+        ryokanType: searchRoomFilter.filter.filterRyokanType || null,
       },
     });
     if (data.length) {
@@ -57,6 +56,9 @@ const Search = () => {
     setLoadingStatus(false);
   }, [searchRoom, searchRoomFilter, documentStart]);
 
+  useEffect(() => {
+    fetchSearchCondition();
+  }, []);
   //* custom hook
   useIntersectionObserver({
     rootElement: itemRootRef.current,
@@ -70,41 +72,49 @@ const Search = () => {
   });
 
   return (
-    <>
-      <div className={`${modalState ? 'filter blur-sm' : ''} `}>
+    <div className={`${modalState ? 'filter blur-sm' : ''} `}>
+      <div>
         <SearchFilter className="text-black flex items-center mx-10 my-1 divide-solid divide-x divide-gray-300 " />
       </div>
-      <div ref={itemRootRef}>
-        {ryokanList.map(
-          ({ amenities, bathrooms, bedrooms, ryokanType, title }, index) =>
-            index === ryokanList.length - 1 ? (
-              <SearchItem
-                bathroomCount={bathrooms.bathCount}
-                bedroomCount={bedrooms.bedroomCount}
-                bedsCount={Object.keys(bedrooms.bedroomList).length}
-                personnel={bedrooms.personnel}
-                ryokanAmenities={amenities}
-                ryokanType={ryokanType}
-                title={title}
-                key={v4()}
-                ref={itemRef}
-              />
-            ) : (
-              <SearchItem
-                bathroomCount={bathrooms.bathCount}
-                bedroomCount={bedrooms.bedroomCount}
-                bedsCount={Object.keys(bedrooms.bedroomList).length}
-                personnel={bedrooms.personnel}
-                ryokanAmenities={amenities}
-                ryokanType={ryokanType}
-                title={title}
-                key={v4()}
-              />
-            )
-        )}
+      <div>
+        <div ref={itemRootRef} className="mt-5">
+          {ryokanList.map(
+            (
+              { amenities, bathrooms, bedrooms, ryokanType, title, photos },
+              index
+            ) =>
+              index === ryokanList.length - 1 ? (
+                <SearchItem
+                  bathroomCount={bathrooms.bathCount}
+                  bedroomCount={bedrooms.bedroomCount}
+                  bedsCount={Object.keys(bedrooms.bedroomList).length}
+                  personnel={bedrooms.personnel}
+                  ryokanAmenities={amenities}
+                  ryokanType={ryokanType}
+                  title={title}
+                  imageUrls={photos}
+                  key={v4()}
+                  ref={itemRef}
+                />
+              ) : (
+                <SearchItem
+                  bathroomCount={bathrooms.bathCount}
+                  bedroomCount={bedrooms.bedroomCount}
+                  bedsCount={Object.keys(bedrooms.bedroomList).length}
+                  personnel={bedrooms.personnel}
+                  ryokanAmenities={amenities}
+                  ryokanType={ryokanType}
+                  imageUrls={photos}
+                  title={title}
+                  key={v4()}
+                />
+              )
+          )}
+          {loadingStatus && <Loader type="Oval" color="#48cfae" />}
+        </div>
+        <div>{/* 지도*/}</div>
       </div>
-      <div>{/* 지도 */}</div>
-    </>
+    </div>
   );
 };
 
--- next_reservation/components/Search/SearchItem.tsx
+++ next_reservation/components/Search/SearchItem.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { amenitiesType } from '@/types/reduxActionTypes/ReduxRegiserRyokanType';
+import Image from 'next/image';
+import React, { useCallback, useRef, useState } from 'react';
+import { v4 } from 'uuid';
+import {
+  amenitiesType,
+  photoType,
+} from '@/types/reduxActionTypes/ReduxRegiserRyokanType';
 import Amenities from '@/lib/staticData/Amenities';
+import { RyokanType } from '@/lib/staticData/RegisterRyokanType';
 
 interface IProps {
   ryokanType: string;
   title: string;
+  imageUrls: photoType[];
   personnel: number;
   bedroomCount: number;
   bedsCount: number;
@@ -17,6 +24,7 @@ const SearchItem = React.forwardRef<any, IProps>(
     {
       ryokanType,
       title,
+      imageUrls,
       personnel,
       bedroomCount,
       bedsCount,
@@ -25,6 +33,19 @@ const SearchItem = React.forwardRef<any, IProps>(
     },
     ref
   ) => {
+    const [visiableImage, setVisiableImage] = useState(0);
+
+    const clickPreviousImageButton = useCallback(() => {
+      if (visiableImage === 0) setVisiableImage(imageUrls.length - 1);
+      if (visiableImage !== 0)
+        setVisiableImage((previousImageNumber) => --previousImageNumber);
+    }, [visiableImage]);
+    const clickNextImageButton = useCallback(() => {
+      if (visiableImage === imageUrls.length - 1) setVisiableImage(0);
+      if (visiableImage !== imageUrls.length - 1)
+        setVisiableImage((previousImageNumber) => ++previousImageNumber);
+    }, [visiableImage]);
+
     const getTruthyAmenities = (amenities: amenitiesType) =>
       (Object.keys(amenities) as Array<keyof amenitiesType>)
         .map((amenityKey) => {
@@ -33,20 +54,63 @@ const SearchItem = React.forwardRef<any, IProps>(
         .filter((result) => result !== undefined);
 
     return (
-      <div ref={ref}>
-        <div>
-          <span data-testid="ryokan-type-text">{ryokanType}</span>
+      <div
+        className="w-1/2 text-black mx-5 p-3 border rounded-2xl flex relative"
+        ref={ref}
+      >
+        <div className="w-1/3 h-300 relative">
+          <button
+            className="w-10 h-10 absolute top-32 left-3 rounded-full bg-emerald text-white border-emerald border z-2 focus:outline-none"
+            onClick={clickPreviousImageButton}
+          >
+            &lt;
+            <span className="hidden">Previous</span>
+          </button>
+          <button
+            className="w-10 h-10 absolute top-32 right-3 rounded-full bg-emerald text-white border-emerald border z-2 focus:outline-none"
+            onClick={clickNextImageButton}
+          >
+            &gt;
+            <span className="hidden">Next</span>
+          </button>
         </div>
-        <div>
-          <span data-testid="ryokan-title-text">{title}</span>
+        <div className="absolute flex">
+          {imageUrls.map((image, index) => (
+            <div className={`${visiableImage === index ? 'block' : 'hidden'}`}>
+              <Image
+                className="rounded-xl"
+                src={image.photoUrl}
+                width="300px"
+                height="300px"
+                layout="fixed"
+                objectFit="cover"
+                objectPosition="center"
+              />
+            </div>
+          ))}
         </div>
-        <div>
-          <span data-testid="ryokan-basic-info">
-            {[personnel, bedroomCount, bedsCount, bathroomCount].join(', ')}
-          </span>
-          <span data-testid="ryokan-amenities">
-            {getTruthyAmenities(ryokanAmenities).join(', ')}
-          </span>
+        <div className="w-3/4 divide-solid divide-y divide-gray-300 flex flex-col justify-center ml-10">
+          <div className="w-full py-3">
+            <span className="text-xl">
+              료칸 유형 : {RyokanType[ryokanType]}
+            </span>
+          </div>
+          <div className="w-full py-3">
+            <span className="inline-block w-full text-3xl truncate">
+              {title}
+            </span>
+          </div>
+          <div className="w-full space-x-2 py-3">
+            <span>최대인원 {personnel}명</span>
+            <span>침실수 {bedroomCount}개</span>
+            <span>침대/침구 수 {bedsCount}개</span>
+            <span>욕실 {bathroomCount}개</span>
+          </div>
+          <div className="w-full py-3">
+            <span data-testid="ryokan-amenities">
+              {getTruthyAmenities(ryokanAmenities).join(', ')}
+            </span>
+          </div>
         </div>
       </div>
     );
