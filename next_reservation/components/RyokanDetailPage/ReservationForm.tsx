fd80c7a631147627ac9a5f73a39792cf0631dd92
oct-sky-out <kms3335k@naver.com>
2022-02-22 17:00:07 +0900

    [기능 제작] 료칸 예약 폼 초안 제작

---
+++ next_reservation/components/RyokanDetailPage/ReservationForm.tsx
@@ -0,0 +1,19 @@
+const ReservationForm = () => {
+  return (
+    <div>
+      <div>
+        <span>예약 일정</span>
+        <div>예약시작 일자</div>
+        <div>예약종료 일자</div>
+      </div>
+      <div>
+        <span>인원수</span>
+      </div>
+      <div>
+        <button>예약하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default ReservationForm;
